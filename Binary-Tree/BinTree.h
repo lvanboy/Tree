e17291bf423f50011c0093f34af1179d6ba5df57
lvanboy <1801996111@qq.com>
2017-08-25 20:53:02 +0800

    Update BinTree.h

--- Binary-Tree/BinTree.h
+++ Binary-Tree/BinTree.h
@@ -1,6 +1,5 @@
 #ifndef _BINTREE_H_
 #define _BINTREE_H_
-
 //the node of binary-tree: binary-link-list-structure
 typedef int ElemData;
 typedef struct BiNode{
@@ -13,8 +12,16 @@ typedef struct BiNode{
 void Preorder(BiTree root);
 void Infixorder(BiTree root);
 void Postorder(BiTree root);
+BiTree GoLeftLeaf(LinkStack *stack, BiTree t);
 //generate binary - tree
 BiTree CreateReCur_BinTree();
+//statistic the num of leaf
+void CountLeafNum(BiTree t,int *sum);
+//get the depth of tree 
+int Get_Depth(BiTree t);
+//copy the tree.
+BiTree CopyTree(BiTree t);
 
 
 #endif
+
